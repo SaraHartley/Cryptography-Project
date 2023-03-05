183542fbf721e63740f3dea80a354909c2b42f27
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2023-03-05 16:46:22 -0600

    Create DES_encyrption.py
    new file

---
+++ DES_encyrption.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  5 16:43:43 2023
+
+DES Encryption
+
+@author: Sarah
+"""
+
+def main():
+    print("Welcome to DES encryption")
+    
+
+if __name__ == "__main__":
+    main()
+
