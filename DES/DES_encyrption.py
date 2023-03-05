c0e12dddd514dc8cb8a5455a031a6cd20230d8b2
SaraHartley <92557187+SaraHartley@users.noreply.github.com>
2023-03-05 17:16:47 -0600

    Update DES_encyrption.py
    1st attempt

--- DES/DES_encyrption.py
+++ DES/DES_encyrption.py
@@ -4,13 +4,22 @@ Created on Sun Mar  5 16:43:43 2023
 
 DES Encryption
 
+pip install pycryptodome
+https://pycryptodome.readthedocs.io/en/latest/src/cipher/des.html
 @author: Sarah
 """
+from Crypto.Cipher import DES
+from Crypto.Util import Padding
 
-def main():
-    print("Welcome to DES encryption")
-    
+
+def main(message):
+    print(message)
+    key = b'-8B key-'
+    cipher = DES.new(key,DES.MODE_ECB)
+    plaintext = Padding.pad(message,8)
+    ciphertext = cipher.encrypt(plaintext)
+    return ciphertext
 
 if __name__ == "__main__":
-    main()
+    print(main("This is a message to encrypt at 4 pm!"))
 
