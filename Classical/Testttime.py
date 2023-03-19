9d8528751cea29be12020e884262000f705dea96
Abdul-Awwal <ibukunadesalu@hotmail.com>
2023-03-19 12:43:19 -0500

    updated test function for caesar

--- Classical/Testttime.py
+++ Classical/Testttime.py
@@ -1,46 +1,83 @@
 import time
 
-def time_func(func, *args, **kwargs):
+def test_decrypt_caesar():
+    # Define a list of ciphertexts to test the function with
+    ciphertexts = [" Zvwopl dhz h ohjrly dov ohk illu dvyrpun vu h wyvqlja mvy tvuaoz& Zol ohk kpzjvclylk h cbsulyhipspaf pu h thqvy zvmadhyl wyvnyht$ huk zol dhz klalytpulk av lewsvpa pa& Zvwopl zwlua jvbuaslzz ovbyz dypapun jvkl$ alzapun oly wyvnyht$ huk mpul%abupun oly zayhalnf& Mpuhssf$ aol khf hyypclk dolu zol dvbsk leljbal oly wshu& Zol pumpsayhalk aol zvmadhyl huk nhpulk hjjlzz av aol zfzalt& Zvwopl.z wyvnyht dvyrlk mshdslzzsf$ huk zol dhz hisl av zalhs chsbhisl khah myvt aol jvtwhuf& Zol mlsa h ybzo vm hkyluhspul hz zol jvtwslalk oly tpzzpvu$ ruvdpun aoha zol ohk qbza hjjvtwspzolk zvtlaopun aoha mld wlvwsl jvbsk& "]
+
+    # Run the function five times for each ciphertext and record the times
     times = []
     for i in range(5):
-        start_time = time.monotonic()
-        func(*args, **kwargs)
-        end_time = time.monotonic()
-        times.append(end_time - start_time)
+        for ciphertext in ciphertexts:
+            start_time = time.time()
+            decrypt_caesar(ciphertext)
+            end_time = time.time()
+            times.append(end_time - start_time)
 
-    return min(times), sum(times) / len(times)
+    # Calculate and print the minimum time taken, average time per call, and total time per five calls
+    min_time = min(times)
+    avg_time = sum(times) / len(times)
+    total_time = sum(times)
+    print(f"Minimum time taken: {min_time:.6f} seconds")
+    print(f"Average time per call: {avg_time:.6f} seconds")
+    print(f"Total time for five calls: {total_time:.6f} seconds")
 
 def decrypt_caesar(cipher_text):
-    for key in range(26):
-        candidate_plain_text = ""
+    decrypted = ''
+    for shift in range(1, 26):
+        candidate_text = ''
         for char in cipher_text:
             if char.isalpha():
-                shifted_char = ord(char) - key
+                shifted_char = ord(char) - shift
                 if char.isupper():
                     if shifted_char < ord('A'):
                         shifted_char += 26
                 else:
                     if shifted_char < ord('a'):
                         shifted_char += 26
-                candidate_plain_text += chr(shifted_char)
+                candidate_text += chr(shifted_char)
             elif char.isdigit():
-                shifted_digit = ord(char) - key
-                if shifted_digit < ord('0'):
-                    shifted_digit += 10
-                candidate_plain_text += chr(shifted_digit)
+                # Shift digits separately from letters
+                shifted_digit = (int(char) - shift) % 10
+                if shifted_digit == 0:
+                    shifted_digit = 10
+                candidate_text += str(shifted_digit)
+            elif '!' <= char <= '/':
+                # Only decrypt ASCII symbols between '!' and '@'
+
+                shifted_char = ord(char) - shift
+                if shifted_char < ord('!'):
+                    shifted_char += 15  # Wrap around to the end of the range
+                candidate_text += chr(shifted_char)
+            elif ':' <= char <= '@':
+                # Only decrypt ASCII symbols between '!' and '@'
+
+                shifted_char = ord(char) - shift
+                if shifted_char < ord(':'):
+                    shifted_char += 7  # Wrap around to the end of the range
+                candidate_text += chr(shifted_char)
+            elif '[' <= char <= '`':
+                # Only decrypt ASCII symbols between '!' and '@'
+
+                shifted_char = ord(char) - shift
+                if shifted_char < ord('['):
+                    shifted_char += 6  # Wrap around to the end of the range
+                candidate_text += chr(shifted_char)
+            elif '{' <= char <= '~':
+                # Only decrypt ASCII symbols between '!' and '@'
+
+                shifted_char = ord(char) - shift
+                if shifted_char < ord('{'):
+                    shifted_char += 4  # Wrap around to the end of the range
+                candidate_text += chr(shifted_char)
             else:
-                candidate_plain_text += char
+                candidate_text += char
+        print(f"key= {shift}, Plain text = {candidate_text}")
+        # Check if the candidate text looks like a valid message
+        if 'the' in candidate_text.lower():
+            decrypted = candidate_text
+            break
 
-
-        # Wrap around the key if it exceeds the range of possible keys
-        if key >= 25:
-            key -= 26
-    return candidate_plain_text
-
+    return decrypted
 # Test the speed of my_function by calling it 5 times with your input
-min_time, avg_time = time_func(decrypt_caesar, "Once upon a time, in a world ruled by technology, there was a brilliant programmer named Max who had a knack for solving complex problems. Max's reputation had spread far and wide, and he had become known as the '@Mastermind!' because of his ability to create incredible software solutions. One day,Max received a message from an unknown sender. The message contained a series of numbers and special characters that seemed to be a code. Max was intrigued and decided to crack the code. After over 40 weeks of working 80 hours on the code, Max discovered that the message was from a mysterious organization that was looking for someone with his skills. They wanted him to create a program that would change the world forever. Max was excited by the challenge and accepted the organization's offer. He worked tirelessly on the program, putting in many sleepless nights to make sure it was perfect. Finally, the day arrived when Max completed the program and presented it to the organization. They were amazed by Max's work and declared him a genius. From that day on, Max became known as the 'Mastermind of the Tech World,' and his legacy lived on for generations to come.")
+test_decrypt_caesar()
 
-# Print the minimum time taken, the average time per call, and the total time taken
-print(f"Minimum time taken: {min_time:.6f} seconds")
-print(f"Average time per call: {avg_time:.10f} seconds")
-print(f"Total time taken for 5 calls: {avg_time * 5:.6f} seconds")
